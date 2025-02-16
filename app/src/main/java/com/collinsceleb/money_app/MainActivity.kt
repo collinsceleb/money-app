ca1253027f76ca8c5b8bcd2a838636f9cee9c26f
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-02-16 12:10:20 +0100

    Update main activity code to launch activities

--- app/src/main/java/com/collinsceleb/money_app/MainActivity.kt
+++ app/src/main/java/com/collinsceleb/money_app/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.collinsceleb.money_app
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowCompat
+import com.collinsceleb.money_app.ui.AccountsActivity
+import com.collinsceleb.money_app.ui.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        if (user != null && user.isEmailVerified) {
+            startActivity(Intent(this, AccountsActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
