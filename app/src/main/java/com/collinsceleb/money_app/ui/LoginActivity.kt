244d9dadeb3fc837e257fe8bf4c50a61331e4f74
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-02-16 11:55:57 +0100

    Add login logic

--- app/src/main/java/com/collinsceleb/money_app/ui/LoginActivity.kt
+++ app/src/main/java/com/collinsceleb/money_app/ui/LoginActivity.kt
@@ -1,15 +1,15 @@
 package com.collinsceleb.money_app.ui
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
-import com.collinsceleb.money_app.R
 import com.collinsceleb.money_app.databinding.ActivityLoginBinding
-import com.collinsceleb.money_app.databinding.ActivityRegisterBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -29,6 +29,48 @@ class LoginActivity : AppCompatActivity() {
                 bottom = systemBars.bottom
             )
             insets
+        }
+        auth = FirebaseAuth.getInstance()
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+            val password = binding.etPassword.text.toString().trim()
+            if (email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(
+                    binding.root,
+                    "Please, kindly fill all the required fields",
+                    Snackbar.LENGTH_LONG
+                ).show()
+                return@setOnClickListener
+            }
+            binding.progressBar.visibility = View.VISIBLE
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                task ->
+                binding.progressBar.visibility = View.GONE
+                if (task.isSuccessful) {
+                    if (auth.currentUser?.isEmailVerified == true) {
+                        startActivity(Intent(this, AccountsActivity::class.java))
+                        finish()
+                    } else {
+                        Snackbar.make(binding.root, "Ensure your email is verified", Snackbar.LENGTH_LONG).show()
+                        auth.signOut()
+                    }
+                } else {
+                    Snackbar.make(binding.root, "Failed login: ${task.exception?.message}", Snackbar.LENGTH_LONG).show()
+                }
+            }
+            binding.btnRegister.setOnClickListener {
+                startActivity(Intent(this, RegisterActivity::class.java))
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null && currentUser.isEmailVerified) {
+            startActivity(Intent(this, AccountsActivity::class.java))
+            finish()
         }
     }
 }
