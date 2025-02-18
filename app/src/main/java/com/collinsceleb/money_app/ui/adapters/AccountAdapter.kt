f7e46e94742f15f3bf59aae7b5d8150f05b5b34b
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-02-18 15:44:19 +0100

    Update account adapter to use binding

--- app/src/main/java/com/collinsceleb/money_app/ui/adapters/AccountAdapter.kt
+++ app/src/main/java/com/collinsceleb/money_app/ui/adapters/AccountAdapter.kt
@@ -1,36 +1,29 @@
 package com.collinsceleb.money_app.ui.adapters
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.collinsceleb.money_app.R
+import com.collinsceleb.money_app.databinding.ItemAccountBinding
 import com.collinsceleb.money_app.model.Account
 
 class AccountAdapter :
     ListAdapter<Account, AccountAdapter.AccountViewHolder>(AccountDiffCallback()) {
 
-    class AccountViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val accountNumberTextView: TextView = itemView.findViewById(R.id.tvAccountNumber)
-        private val accountNameTextView: TextView = itemView.findViewById(R.id.tvAccountHolder)
-        private val accountTypeTextView: TextView = itemView.findViewById(R.id.tvAccountType)
-        private val accountBalanceTextView: TextView = itemView.findViewById(R.id.tvBalance)
-
+    class AccountViewHolder(private val binding: ItemAccountBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(account: Account) {
-            accountNumberTextView.text = account.accountNumber
-            accountNameTextView.text = account.accountHolder
-            accountTypeTextView.text = account.accountType
-            accountBalanceTextView.text = String.format(account.accountBalance.toString())
+            binding.tvAccountNumber.text = account.accountNumber
+            binding.tvAccountHolder.text = account.accountHolder
+            binding.tvAccountType.text = account.accountType
+            binding.tvBalance.text = String.format(account.accountBalance.toString())
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AccountViewHolder {
-        val itemView =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_account, parent, false)
-        return AccountViewHolder(itemView)
+        val binding =
+            ItemAccountBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AccountViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: AccountViewHolder, position: Int) {
